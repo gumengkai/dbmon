1a0509bd901c562f7332bbbc8c938030e53e0e1c
gumengkai <gumengkai@hotmail.com>
2018-11-21 19:14:15 +0800

    新增webssh功能，在Linux概览面板输入用户名密码可以直接进行ssh登录

---
+++ webssh/webssh/static/js/getUrl.js
@@ -0,0 +1,8 @@
+/*获取到Url里面的参数*/
+(function ($) {
+  $.getUrlParam = function (name) {
+   var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+   var r = window.location.search.substr(1).match(reg);
+   if (r != null) return unescape(r[2]); return null;
+  }
+ })(jQuery);
